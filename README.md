a2731b9caee24f46169134854b7ebc2897a99bc3
Menna <Menna.Tullah.magdy@outlook.com>
2015-12-14 23:09:49 +0200

    ~

---
+++ README.md
@@ -0,0 +1,23 @@
+# Training Project: calculator
+This is a calculator
+It is supposed to take commands on the form of `awesomecalculator add 3 5` <br/>
+and the output would be `8` <br/>
+so `argv[1]` is the operation. ex. add, subtract, multiply, divide, absolute, negate, power, root, log, ln, factorial, sin, cos and tan.<br/>
+`argv[2]` and `argv[3]` are the operands.<br/>
+everyone can implement one operation function.<br/>
+and someone will implement the main function body.<br/>
+if all functions were already implemented, come up with a new one and make it :D
+
+## People who have contributed to the project so far:
+<ol>
+<li>Eslam Romario</li>
+<li>Mohamed Medhat</li>
+<li>Omar Alsawaf</li>
+<li>Abdo Alaa</li>
+<li>Yasmin Ghazy</li>
+<li>Yara Yasser</li>
+<li>Saieed Osama</li>
+<li>Omneya Yahya</li>
+<li>Sara Rabie</li>
+<li>Omar M. Ali</li>
+</ol>
---
+++ operations.h
@@ -0,0 +1,130 @@
+#include <math.h>
+
+using namespace std;
+
+#define PI 3.14159265
+
+float _tan (float num1)
+{
+	float result =tan(num1*PI/180);
+
+	return result;
+}
+
+float subtract(float num1, float num2)
+{
+	float result = num1-num2 ;
+	return result;
+}
+
+
+float _sin(float rate)
+{
+    float result,r;
+    r=rate/180;
+    int i=r;
+    if (i-r==0)
+    {
+        result=0;
+    }
+    else
+    {
+        result = sin ((rate*PI)/180);
+    }
+    return result;
+}
+
+
+double _log(double x)
+{
+    return (log10(x));
+}
+
+double _log(double base, double x) {
+    return (double)(log(x) / log(base));
+}
+
+double powerfunction(double base, double power){
+
+	double f = base;
+	for (int i = 1; i < power; i++)
+		f *= base;
+	return f;
+}
+
+
+float factorial(float n)
+{
+
+    float result=1;
+
+
+     for (float i =1;i<=n;i++){
+         result*=i;}
+
+    return result;
+}
+
+double _negate (double num){
+	return num*(-1);
+}
+
+double add (double n1 , double n2){
+	return n1+n2;
+}
+
+double root (double num){
+	return sqrt(num);
+}
+
+double divide(double n1 , n2){
+	if(n2!=0)
+	    return n1/n2;
+	else cout<<"AN error";
+	return 0;
+}
+
+double absolute (double num){
+    return (num<0?-1*num:num);
+}
+
+unsigned long long factorial(double num){
+	unsigned long long fac=1;
+	for(int i = 1 ; i <= num ; i++)
+	    fac*=i;
+	return fac;
+}
+
+long double ln(long double x)
+{
+   //***********Variable Declarations************/
+   long double count = 0;     //Keeps track of the count in the loop
+   long double totalValue = 0; //The summation of each polynomial evaluated
+   bool reciprocal = false;   //Flag to use if greater than 2
+   long double precision = 9;     //The the highest order of polynomial to use.
+
+   if (x > 2.0)   //Use the rule -ln(1/x) == ln(x) to keep accuracy
+   {
+      x = 1 / x;        //Change to using 1/x rather than x
+      reciprocal = true; //Flag as true (sign change is later)
+   }
+
+   while (count < precision)
+   {
+      totalValue += pow(-1, count + 1) * (pow((x-1),count)/count);
+      count++;
+   }
+
+   if (reciprocal == true)
+      totalValue * -1;   //If reciprocal was used multiply by -1 to change sign
+
+   return totalValue;
+}
+
+double divide(double num1, double num2)
+{
+if(num2 == 0)
+cout<<"Invalid, the second number must be > 0"<<endl;
+double result = num1/num2;
+return result;
+}
